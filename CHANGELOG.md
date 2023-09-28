d070a9d8cf0b71fbbc497c832b540c317a46f66e
dingogap <petermedbury@Betelgeuse.local>
2023-09-28 11:00:46 +1000

    Updated User Route & userController to remove a Friend

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,6 @@
 5. Updated Thoughts model to include Reactions
 6. Add User and Thought Routes
 7. Added Controllers - some not ready & so commented out
-8. Updated User Route & userController to add thought and update user
-9. Updated User Route & userController to add Friend
+8. Updated User Route & userController to add Thought and update user
+9. Updated User Route & userController to add a Friend
+10. Updated User Route & userController to remove a Friend
--- controllers/userController.js
+++ controllers/userController.js
@@ -75,4 +75,26 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  // remove a friend
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
 };
